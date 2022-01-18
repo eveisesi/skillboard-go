0e5873c19ca9db0122d4e67191112fde38b2819e
David Douglas <david@onetwentyseven.dev>
2022-01-18 02:51:49 +0000

    Remove unused scopes Remove contacts. Will replace with NPC Standing in v2.

--- scopes.go
+++ scopes.go
@@ -30,20 +30,15 @@ type ScopeResolver struct {
 type Scope string
 
 const (
-	ReadContactsV1   Scope = "esi-characters.read_contacts.v1"
 	ReadClonesV1     Scope = "esi-clones.read_clones.v1"
 	ReadImplantsV1   Scope = "esi-clones.read_implants.v1"
-	ReadLocationV1   Scope = "esi-location.read_location.v1"
-	ReadOnlineV1     Scope = "esi-location.read_online.v1"
-	ReadShipV1       Scope = "esi-location.read_ship_type.v1"
 	ReadSkillQueueV1 Scope = "esi-skills.read_skillqueue.v1"
 	ReadSkillsV1     Scope = "esi-skills.read_skills.v1"
 	ReadStructuresV1 Scope = "esi-universe.read_structures.v1"
 )
 
 var AllScopes = []Scope{
-	ReadImplantsV1, ReadClonesV1, ReadContactsV1,
-	ReadLocationV1, ReadOnlineV1, ReadShipV1,
+	ReadImplantsV1, ReadClonesV1,
 	ReadSkillQueueV1, ReadSkillsV1, ReadStructuresV1,
 }
 
