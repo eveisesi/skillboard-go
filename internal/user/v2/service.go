6ffad2b091dae5db3fb8f5ccc97721262c63888d
David Douglas <david@onetwentyseven.dev>
2022-04-01 21:22:53 +0000

    Some more logging

--- internal/user/v2/service.go
+++ internal/user/v2/service.go
@@ -475,9 +475,16 @@ func (s *Service) UserFromToken(ctx context.Context, token jwt.Token) (*skillz.U
 
 	userID := hashedUserID(ownerHash, strconv.FormatUint(characterID, 10))
 
+	entry := s.logger.WithFields(logrus.Fields{
+		"service":     "user.UserFromToken",
+		"ownerHash":   ownerHash,
+		"characterID": characterID,
+		"userID":      userID,
+	})
+	entry.Info("UserFromToken params")
+
 	user, err := s.UserRepository.UserByCharacterID(ctx, characterID)
 	if err != nil && errors.Is(err, sql.ErrNoRows) {
-
 		user = &skillz.User{
 			ID:          userID,
 			CharacterID: characterID,
@@ -490,6 +497,8 @@ func (s *Service) UserFromToken(ctx context.Context, token jwt.Token) (*skillz.U
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to locate user for this token in our database")
 	}
+
+	entry.WithField("user", user).Info("userByCharacterID")
 
 	character, err := s.character.Character(ctx, user.CharacterID)
 	if err != nil {
